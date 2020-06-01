bf5dc7c850437c464beeabd88b8076c48d923c7d
unknown <klam9986@bths.edu>
2020-06-01 16:03:09 -0400

    Updated files of both index and js

--- Assign4.js
+++ Assign4.js
@@ -1,3 +1,5 @@
+// QUESTION 1
+
 function talker1(){
     tTalk1 = document.getElementById("talk1").innerHTML = "I'm right";
 }
@@ -5,3 +7,13 @@ function talker1(){
 function talker2(){
     tTalk1 = document.getElementById("talk1").innerHTML = "No, I'm right!";
 }
+
+// QUESTION 2
+
+
+// function myHover(element) {
+//     element.
+// }
+document.getElementById("hover-area").addEventListener("mouseover", function(e) {
+    alert("Hey, I told you not to hover over me!");
+});
