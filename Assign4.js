b16b6dae72150c4457c298da848f27fb86aad3e1
Manahil Sher <manahil.sher@gmail.com>
2020-06-01 16:08:39 -0400

    completed question 4

--- Assign4.js
+++ Assign4.js
@@ -16,15 +16,11 @@ document.getElementById("hover-area").addEventListener("mouseover", function(e) 
 
 // QUESTION 4
 
-// In a Javascript file, write a program which checks the following things:
-// checks that the password is 12345678
-// if the password is incorrect, send an alert message
-// Your page should also include an <h1> tag. If the information in the form is correct, have Javascript change the text in the <h1>.HTML page.
-
-function validation(password) {
+document.getElementById("login").addEventListener("submit", function(e) {
+    let password = document.getElementById("password").value;
     if (password != "12345678") {
         alert("Incorrect Password!")
     } else {
-        let text = document.getElementById("login").innerText = "Successfully logged in!"
+        document.getElementById("login").innerText = "Successfully logged in!"
     }
-}
+});
