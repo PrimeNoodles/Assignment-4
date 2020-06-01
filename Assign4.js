17e3ace6059db2991a4ea18489999a41d4427432
unknown <klam9986@bths.edu>
2020-06-01 14:38:15 -0400

    Added javascript file and basic modifications to index.html

---
+++ Assign4.js
@@ -0,0 +1,7 @@
+function talker1(){
+    tTalk1 = document.getElementById("talk1").innerHTML = "I'm right";
+}
+
+function talker2(){
+    tTalk1 = document.getElementById("talk1").innerHTML = "No, I'm right!";
+}
